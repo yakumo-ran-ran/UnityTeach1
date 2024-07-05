af71bd035020c13cffb30179fb03c759c35b7860
Yakumo <101714689+yakumo-ran-ran@users.noreply.github.com>
2024-07-05 21:12:52 +0800

    Input练习题

---
+++ Assets/Scripts/Lesson11输入相关Input/Lesson11Ex.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lesson11Ex : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public float rotateSpeed;
+
+    public float headRotateSpeed;
+
+    public Transform head;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input.GetAxis("Horizontal");
+        this.transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime*Input.GetAxis("Vertical"),Space.Self);
+        this.transform.Rotate(Vector3.up*rotateSpeed*Time.deltaTime,Input.GetAxis("Horizontal"),Space.Self);
+        head.Rotate(Vector3.up*headRotateSpeed*Time.deltaTime*Input.GetAxis("Mouse X"),Space.Self);
+    }
+}
