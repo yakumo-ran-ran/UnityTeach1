fb2bec52dc1abd4ad135ba3060511c5422b2505f
Yakumo <101714689+yakumo-ran-ran@users.noreply.github.com>
2024-07-05 20:29:39 +0800

    Input鼠标键盘输入
    input相关

---
+++ Assets/Scripts/Lesson11输入相关Input/Lesson11.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lesson11 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // // print(Input.mousePosition);
+        // if (Input.GetMouseButtonDown(0))
+        // {
+        //     print("left mouse click");
+        // }
+        //
+        // if (Input.GetMouseButtonUp(0))
+        // {
+        //     print("left mouse up");
+        // }
+        //
+        // if (Input.GetMouseButton(0))
+        // {
+        //     print("left mouse press");
+        // }
+        //
+        // // print(Input.mouseScrollDelta);
+        //
+        // if (Input.GetKeyDown(KeyCode.W))
+        // {
+        //     print("W was pressed");
+        // }
+        //
+        // if (Input.GetKeyDown("q"))
+        // {
+        //     print("Q was pressed");
+        // }
+        //
+        // if (Input.GetKeyUp("q"))
+        // {
+        //     print("Q was up");
+        // }
+        // if (Input.GetKeyUp("q"))
+        // {
+        //     print("Q was pressing");
+        // }
+        // if (Input.GetKeyDown("space"))
+        // {
+        //     print("Space was pressed");
+        // }
+
+        // print(Input.GetAxis("Horizontal"));
+        // print(Input.GetAxis("Vertical"));
+        print(Input.GetAxis("Mouse X"));
+        
+    }
+}
